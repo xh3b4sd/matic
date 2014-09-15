2ffbad6d9d1b7b0bfb24227035c1473f56b059a7
Tim Schindler <tim@catalyst-zero.com>
2014-09-15 21:03:59 +0000

    moved private methods to the file where they are used

--- collector/source_code.go
+++ collector/source_code.go
@@ -1,6 +1,7 @@
 package collector
 
 import (
+	"io/ioutil"
 	"os"
 	"path/filepath"
 )
@@ -61,4 +62,35 @@ func SourceCodeTask(ctx interface{}) error {
 	}
 
 	return nil
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// private
+
+func skipFile(ext, path string, info os.FileInfo) bool {
+	// Skip directories.
+	if info.IsDir() {
+		return true
+	}
+
+	// Skip none go files.
+	if filepath.Ext(path) != "."+ext {
+		return true
+	}
+
+	return false
+}
+
+func readFile(path string) (string, error) {
+	reader, err := os.Open(path)
+	if err != nil {
+		return "", Mask(err)
+	}
+
+	byteSlice, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return "", Mask(err)
+	}
+
+	return string(byteSlice), nil
 }
